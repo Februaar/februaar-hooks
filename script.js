30d5ea4057674b466036607db53dc55cf6b85cc3
Kyungjin Jung <kyoooungjin@gmail.com>
2025-07-10 01:27:15 +0900

    useState hook

--- script.js
+++ script.js
@@ -12,17 +12,17 @@ function useState(initialValue) {
 
   function setState(newValue) {
     state = newValue;
-    render(); // UI를 업데이트하는 역할
+    render(); // UI 업데이트 역할
   }
 
   return [() => state, setState];
 }
 
-// 상태 및 UI 업데이트 함수 만들기기
+// 상태 및 UI 업데이트 함수 만들기
 const [getCount, setCount] = useState(0);
 
 function render() {
-  document.getElementById("count").innerText = getCount();
+  document.getElementById("count").innerText = getCount(); // 함수 결과 값
 }
 
 document.getElementById("btn").addEventListener("click", () => {
@@ -31,21 +31,21 @@ document.getElementById("btn").addEventListener("click", () => {
 
 render(); // 초기 UI 렌더링
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
-};
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return { count: state.count + 1 };
+//     case "decrement":
+//       return { count: state.count - 1 };
+//     default:
+//       return state;
+//   }
+// };
 
-const [state, dispatch] = useReducer(reducer, { count: 0 });
+// const [state, dispatch] = useReducer(reducer, { count: 0 });
 
-dispatch({ type: "increment" });
+// dispatch({ type: "increment" });
 
-useEffect(() => {
-  fetchData();
-}, []); // 의존성 배열이 비어 있으면 마운트 시 1회 실행
+// useEffect(() => {
+//   fetchData();
+// }, []); // 의존성 배열이 비어 있으면 마운트 시 1회 실행
